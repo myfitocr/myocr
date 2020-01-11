eaffd130fea4078700ceaeeb038f7160eb063a9e
ddiddu <59246296+ddiddu@users.noreply.github.com>
2020-01-12 00:59:14 +0900

    pantsActivity까지

--- app/src/main/java/com/example/myfit/MainActivity.java
+++ app/src/main/java/com/example/myfit/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.myfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Message;
+import android.util.Size;
 import android.widget.TextView;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
@@ -18,10 +20,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TessBaseAPI tess;
     String dataPath = "";
+    ArrayList<SizeClass> sizeClasses=new ArrayList<>();
+    SizeClass sizeClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +44,59 @@ public class MainActivity extends AppCompatActivity {
         bitmap = ARGBBitmap(bitmap);
         processImage(bitmap);
     }
+
     public void processImage(Bitmap bitmap){
         String result = null;
         tess.setImage(bitmap);
         result = tess.getUTF8Text();
         TextView textView = findViewById(R.id.text);
         textView.setText(result);
+
+        String target="보세요\n";
+        int target_num=result.indexOf(target);
+        String size;
+        size= result.substring(target_num+4);
+
+        char endOfSize=getEndOfSize(size);
+        int endOfSize_num=size.indexOf(endOfSize);
+        String getSize=size.substring(0,endOfSize_num);
+        sizeClasses=getSizeInfo(getSize);
+        Intent intent=new Intent(getApplicationContext(),PantsActivity.class);
+        intent.putExtra("sizeInfo",sizeClasses);
+        startActivity(intent);
     }
+
+    private char getEndOfSize(String size){
+        for(char c:size.toCharArray()){
+            if (c>=32 && c<=126 || c==10){
+            }else {
+                return c;
+            }
+        }
+        return 0;
+    }
+
+    private ArrayList<SizeClass> getSizeInfo(String getSize){
+        String[] array=getSize.split("\n");
+        String[] getType=array[0].split(" ");
+
+        if(getType.length==6){//바지
+            for(int i=0;i<array.length;i++){
+                String[] getSizeInfo=array[i].split(" ");
+                sizeClass=new SizeClass(getSizeInfo[0]);
+
+                ArrayList<Float> info=new ArrayList<>();
+                for(int j=1;j<getSizeInfo.length;j++){
+                    info.add(Float.parseFloat(getSizeInfo[j]));
+                }
+
+                sizeClass.setSizeInfo(info);
+                sizeClasses.add(sizeClass);
+            }
+        }
+        return sizeClasses;
+    }
+
     private void checkFile(File dir,String lang){
         if(!dir.exists()&&dir.mkdirs()){
             copyFiles(lang);
@@ -58,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     private void copyFiles(String lang) {
         try {
             String filepath = dataPath + "/tessdata/" + lang + ".traineddata";
@@ -79,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
     private Bitmap ARGBBitmap(Bitmap img) {
         return img.copy(Bitmap.Config.ARGB_8888,true);
     }
--- app/src/main/java/com/example/myfit/PantsActivity.java
+++ app/src/main/java/com/example/myfit/PantsActivity.java
@@ -2,24 +2,57 @@ package com.example.myfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class PantsActivity extends AppCompatActivity {
     ArrayList<SizeClass> sizeClasses=new ArrayList<>();
+    ArrayList<String> items=new ArrayList<>();
+    private String text;
+    ArrayList<SizeClass> biggerSize=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pants);
+        final Spinner sizeSpinner=findViewById(R.id.sizeSpinner);
 
         //ocrActivity에서 사이즈 정보 받아오기
         Intent i=getIntent();
         Bundle bundle=i.getExtras();
-
         sizeClasses= (ArrayList<SizeClass>) bundle.get("sizeInfo");
-        System.out.println("pantsActivity"+sizeClasses.size());
+
+        for (int j=0;j<biggerSize.size();j++){
+            items.add(biggerSize.get(j).getSizeName());
+        }
+
+        setspinner(sizeSpinner,items);
+    }
+
+    public String setspinner(final Spinner spinner,ArrayList<String> items){
+        ArrayList<String> items_1 = items;
+        ArrayAdapter myadapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,items_1);
+        spinner.setAdapter(myadapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        //spinner 값 받아오는 부분 다시 짜기
+        return text;
     }
 }
